cc2b0b5c9ecb05db4b07684ffdb066c847ffde75
jessebutterfield <jesse.butterfield@gmail.com>
2016-08-31 00:24:01 -0700

    First texts sent

--- texts/views.py
+++ texts/views.py
@@ -2,20 +2,23 @@ from django.http import HttpResponse
 from twilio.rest import TwilioRestClient
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
+from texts.models import TwilioNumber, Contact
 import sys
 
 client = TwilioRestClient(account=settings.TWILIO_ACCOUNT_SID, token=settings.TWILIO_AUTH_TOKEN)
 
 
 def index(request):
-    if not request.user.is_authenticated():
-        print(request.META)
-        return HttpResponse('', status=401)
-    print(request.user)
     return HttpResponse("Hello, world. You're at the texts index.")
 
 @csrf_exempt
 def reply(request):
-    if not request.user.is_authenticated:
-        return HttpResponse('', status=401)
+    to = request.POST['To']
+    numbers = TwilioNumber.objects.filter(number=to)
+    message_body = request.POST['Body']
+    target = message_body.split()[0]
+    contact = Contact.objects.filter(user=numbers[0], first_name=target)
+    message = client.messages.create(to=contact[0], from_=to,
+                                     body="Hello there!")
+    number = request.POST['From']
     return HttpResponse("")
