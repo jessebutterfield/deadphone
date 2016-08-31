2306bc3bd2610e8578daafe716a479be0b443e2b
jessebutterfield <jesse.butterfield@gmail.com>
2016-08-30 18:01:23 -0700

    Update with csrf exempt

--- texts/views.py
+++ texts/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from twilio.rest import TwilioRestClient
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 import sys
 
 client = TwilioRestClient(account=settings.TWILIO_ACCOUNT_SID, token=settings.TWILIO_AUTH_TOKEN)
@@ -8,6 +9,7 @@ client = TwilioRestClient(account=settings.TWILIO_ACCOUNT_SID, token=settings.TW
 def index(request):
     return HttpResponse("Hello, world. You're at the texts index.")
 
+@csrf_exempt()
 def reply(request):
     print(request.body, file=sys.stderr)
     return HttpResponse("")
