4ef8fd988ccd7abea4bf86f4311041cf63725fe1
jessebutterfield <jesse.butterfield@gmail.com>
2016-08-30 17:52:44 -0700

    Fix urls

--- texts/views.py
+++ texts/views.py
@@ -1,7 +1,5 @@
 from django.http import HttpResponse
-import os
 from twilio.rest import TwilioRestClient
-import twilio.twiml
 from django.conf import settings
 
 client = TwilioRestClient(account=settings.TWILIO_ACCOUNT_SID, token=settings.TWILIO_AUTH_TOKEN)
